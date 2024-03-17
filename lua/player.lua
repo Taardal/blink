687e3b5d68188b602e73f13e31a312097eb4d845
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2024-03-17 23:57:09 +0100

    Updated player lua script

--- lua/player.lua
+++ lua/player.lua
@@ -1,4 +1,4 @@
-local moveSpeed = 2.0
+local moveSpeed = 5.0
 
 function Player.onUpdate(entityId, timestep)
   print("Player.onUpdate")
@@ -8,16 +8,16 @@ function Player.onUpdate(entityId, timestep)
 
   local position = Entity:getPosition(entityId)
   local velocity = moveSpeed * timestep
-  if (Keyboard:isPressed(Key.Numpad_4)) then
+  if (Keyboard:isPressed(Key.H) or Keyboard:isPressed(Key.Numpad_4)) then
       position.x = position.x - velocity
   end
-  if (Keyboard:isPressed(Key.Numpad_6)) then
+  if (Keyboard:isPressed(Key.L) or Keyboard:isPressed(Key.Numpad_6)) then
       position.x = position.x + velocity
   end
-  if (Keyboard:isPressed(Key.Numpad_5)) then
+  if (Keyboard:isPressed(Key.J) or Keyboard:isPressed(Key.Numpad_5)) then
       position.y = position.y - velocity
   end
-  if (Keyboard:isPressed(Key.Numpad_8)) then
+  if (Keyboard:isPressed(Key.K) or Keyboard:isPressed(Key.Numpad_8)) then
       position.y = position.y + velocity
   end
   Entity:setPosition(entityId, position)
